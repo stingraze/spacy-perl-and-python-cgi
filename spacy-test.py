676ae731c13871103d8b84756a21ab4b240c7103
Tsubasa Kato <tsubasa@superai.online>
2020-06-01 11:38:14 +0900

    Added array and sort function

--- spacy-test.py
+++ spacy-test.py
@@ -1,8 +1,9 @@
 #(C)Inspire Search 2020/5/31 Coded by Tsubasa Kato (@_stingraze)
+#Last edited on 2020/6/1 11:36AM JST
 import sys
 import spacy
 import re
-
+#gets query from argv[1]
 text = sys.argv[1]
 
 nlp = spacy.load('en_core_web_sm')
@@ -10,6 +11,11 @@ doc = nlp(text)
 
 ahref = "<a href=\""
 ahref2 = "\"\>"
+
+#arrays for storing subject and object types 
+subj_array = []
+obj_array = []
+
 for d in doc:
 	#print((d.text, d.pos_, d.dep_))
 	word = d.text
@@ -17,13 +23,32 @@ for d in doc:
 	dep = d.dep_
 #If it matches subject, do this
 	if re.search(r'subj', dep):
+		
 		word2 = ahref + 'http://www.superai.online/solr/search.php?query='+ word + ahref2 + word + '</a>'
+		subj_array.append(word)
 		print (word2)
 		print (pos)
 		print (dep)
+
 #If it matches object, do this
 	if re.search(r'obj', dep):
 		word2 = ahref + 'http://www.superai.online/solr/search.php?query='+ word + ahref2 + word + '</a>'
+		obj_array.append(word)
 		print (word2)
 		print (pos)
 		print (dep)
+
+
+#Sorts both arrays
+#ToDo & Note to self: 
+#Study more of sorting so I can visualize this as table etc.
+subj_array.sort()
+obj_array.sort()
+for subj in subj_array:
+		print (subj)
+
+for obj in obj_array:
+		print (obj)
+
+
+
